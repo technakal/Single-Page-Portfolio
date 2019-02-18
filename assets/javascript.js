98743f1a5acbe570ddf11b0fd21989eb3bce861d
Brant Keener <brant_keener@yahoo.com>
2019-02-18 16:32:22 -0700

    Add different projects to portfolio, add some hover effects

--- assets/javascript.js
+++ assets/javascript.js
@@ -34,65 +34,61 @@ let projects = {
 
 let portfolioPosition = 0;
 
-// Event delegator for mouse clicks
-document.addEventListener('click', event => {
-    let listen = '';
-    if(event.target.className.baseVal) {
-        listen = event.target.className.baseVal;
-    } else {
-        listen = event.target.id;
-    };
-    switch(listen) {
-        case 'primary right_arrow':
-        case 'secondary right_arrow':
-        moveCards('right');
-        break;
-        case 'primary left_arrow':
-        case 'secondary left_arrow':
-        moveCards('left');
-        break;
+// Ensure that the document is loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Event delegator for mouse clicks
+    document.addEventListener('click', event => {
+        let listen = '';
+        if(event.target.className.baseVal) {
+            listen = event.target.className.baseVal;
+        } else {
+            listen = event.target.id;
+        };
+        switch(listen) {
+            case 'primary right_arrow':
+            case 'secondary right_arrow':
+            moveCards('right');
+            break;
+            case 'primary left_arrow':
+            case 'secondary left_arrow':
+            moveCards('left');
+            break;
+        };
+    });
+
+    function moveCards(direction) {
+        // Each project has a data-active state. 
+        // If this state is true on either of the end cards, it prevents
+        // Movement in that direction.
+        let change = false;
+        if(direction === 'right' && projects.project6.state !== true) {
+            console.log(portfolioPosition);
+            portfolioPosition -= 650;
+            let positioner = `${portfolioPosition.toString()}px`;
+            for(let i = 1; i <= Object.keys(projects).length; i++) {
+                let cards = document.getElementById(`project${[i]}`);
+                let projectCount = `project${[i]}`;
+                cards.style.left = positioner;
+                if(projects[projectCount].state === true && change === false) {
+                    change = true;
+                    projects[projectCount].state = false;
+                    projects[`project${[i + 1]}`].state = true;
+                };
+            };
+        } else if(direction === 'left' && projects.project1.state !== true) {
+            portfolioPosition += 650;
+            let positioner = `${portfolioPosition.toString()}px`;
+            for(let i = 1; i <= Object.keys(projects).length; i++) {
+                let cards = document.getElementById(`project${[i]}`);
+                let projectCount = `project${[i]}`;
+                cards.style.left = positioner;
+                if(projects[projectCount].state === true && change === false) {
+                    change = true;
+                    projects[projectCount].state = false;
+                    projects[`project${[i - 1]}`].state = true;
+                };
+            };
+        };
     };
 });
-
-function moveCards(direction) {
-    console.log(projects.project1.state);
-    let change = false;
-    if(direction === 'right' && projects.project6.state !== true) {
-        portfolioPosition -= 650;
-        let positioner = `${portfolioPosition.toString()}px`;
-        for(let i = 1; i <= Object.keys(projects).length; i++) {
-            let cards = document.getElementById(`project${[i]}`);
-            let projectCount = `project${[i]}`;
-            cards.style.left = positioner;
-            if(projects[projectCount].state === true && change === false) {
-                change = true;
-                projects[projectCount].state = false;
-                projects[`project${[i + 1]}`].state = true;
-            };
-        };
-    } else if(direction === 'left' && projects.project1.state !== true) {
-        portfolioPosition += 650;
-        let positioner = `${portfolioPosition.toString()}px`;
-        for(let i = 1; i <= Object.keys(projects).length; i++) {
-            let cards = document.getElementById(`project${[i]}`);
-            let projectCount = `project${[i]}`;
-            cards.style.left = positioner;
-            if(projects[projectCount].state === true && change === false) {
-                change = true;
-                projects[projectCount].state = false;
-                projects[`project${[i - 1]}`].state = true;
-            };
-        };
-    };
-    hideShowCards();
-};
-
-function hideShowCards() {
-    for(let prop in projects) {
-        if(!projects[prop]) {
-            document.getElementById(prop).style.visibility = 'hidden';
-        } else {
-            document.getElementById(prop).style.visibility = 'visible';
-        }
-    };
-};
